9b6249349feac39dc7c2c12407bb1e19e373d2c5
jakelovescoding <97002961+jakelovescoding@users.noreply.github.com>
2022-11-28 12:00:00 -0600

    added comments and space

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ import java.util.*;
 class Main {
   public static void main(String[] args) {
     Scanner reader = new Scanner(System.in);  // Reading from System.in
+    
     System.out.println("Capitalized letters? true / false");
     boolean capitalFlag = reader.nextBoolean();
     System.out.println("Numbers? true / false");
@@ -11,8 +12,11 @@ class Main {
     boolean symbolsFlag = reader.nextBoolean();
     System.out.println("Password length?");
     int passLength = reader.nextInt();
+    
     reader.close();
+    
     String alphabet = "abcdefghijklmnopqrstuvwxyz";
+    
     if (capitalFlag){
       alphabet += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     }
@@ -23,10 +27,11 @@ class Main {
       alphabet+="!@#$%^&*()_+{}|:<>?";
     }
     String password = "";
+    
     for (int i=0; i<passLength; i++){
       Random rand = new Random();
-      password += alphabet.charAt(rand.nextInt(alphabet.length() + 1));
+      password += alphabet.charAt(rand.nextInt(alphabet.length() + 1)); //Add a random character from the alphabet
     }
-    System.out.println(password);
+    System.out.println("Password: "+password);
   }
 }
